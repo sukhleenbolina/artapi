a975d950b3a3e4625039e2a69762d6d5288051f7
Sukhleen <sbolina@eagles.ewu.edu>
2019-11-17 13:39:52 -0800

    Added Content

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,55 @@
 
 
 console.log('ok')
+
+/************
+* Where is the data (api feed)? 
+************/
+// The API feed
+const getStuff = 'https://opentdb.com/api.php?amount=10&category=25&type=multiple';
+
+/************
+* Show the Data
+************/ 
+// Show the data
+const showStuff = (data) => {
+    
+  // Check the data   
+  console.log(data);
+  // How much data is there?
+  console.log(data.length);
+  // How do you access a single data entry? 
+  console.log(data[0]);
+  
+  // Show single entry, then repeat (loop-through) one-at-a-time 
+  for (i=0; i < data.results.length; i++) { 
+     
+    
+      // Template for data. Have fun. Be a Designer.
+      // jQuery for easy DOM manipulations: `text()`, `attr()`, `addClass()`, etc. 
+           // Template.
+      let template =
+          `
+<aside> 
+           <section class="question">
+            <p>${data.results[i].question}</p>
+          
+</section>
+ <section class="answer">
+
+     <p class="animation">${data.results[i].correct_answer}</p>
+           </section>
+
+</aside> `; 
+    
+ 
+    
+      // Show Data Within Template.
+      $('#app').append(template);
+    
+  } // End of Loop.
+    
+}; // End of 'showStuff' Function.
+
+// JSON From API. 
+$.getJSON( getStuff, showStuff);
